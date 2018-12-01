d32e2313a599bfee86fd0cbcf5ca4e679c8d3f07
ellymaimon <eliranmaimon@gmail.com>
2018-11-30 17:53:26 -0800

    Add Game Screen to conditional render in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import SplashScreen from './SplashScreen';
 import { Container } from 'semantic-ui-react';
+import GameScreen from './GameScreen';
 
 const App = () => {
   const [game, setGame] = useState(false);
   
   return (
     <Container style={{ marginTop: '2em' }}>
-    {game ? <h1>Started Game.</h1> :  <SplashScreen setGame={setGame} />}
+    {game ? <GameScreen /> :  <SplashScreen setGame={setGame} />}
     </Container>
   );
 };
