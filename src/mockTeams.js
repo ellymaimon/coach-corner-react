226e5364b3c7998617538128bd8da3df77296e56
ellymaimon <eliranmaimon@gmail.com>
2018-11-30 17:52:09 -0800

    Add mock teams, including methods to generate random names and stats

---
+++ src/mockTeams.js
@@ -0,0 +1,37 @@
+import faker from 'faker';
+import v4 from 'uuid';
+
+const getFirstName = () => {
+  return faker.fake('{{name.firstName}}');
+};
+
+const getLastName = () => {
+  return faker.fake('{{name.lastName}}');
+};
+
+const getRandom = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const createPlayer = () => {
+  let player = {
+    id: v4(),
+    firstName: getFirstName(),
+    lastName: getLastName(),
+    offense: getRandom(0, 10),
+    defense: getRandom(0, 10),
+    stamina: 10
+  };
+  return player;
+};
+
+export const teams = {
+  home: {
+    active: [createPlayer(), createPlayer(), createPlayer()],
+    bench: [createPlayer(), createPlayer(), createPlayer()]
+  },
+  away: {
+    active: [createPlayer(), createPlayer(), createPlayer()],
+    bench: [createPlayer(), createPlayer(), createPlayer()]
+  }
+};
