f8455a6134c4b8ef91331c266bfbcea1a06c2aa5
ellymaimon <eliranmaimon@gmail.com>
2018-11-30 17:54:05 -0800

    Add Bench Players UI Skeleton

---
+++ src/BenchPlayers.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Grid } from 'semantic-ui-react';
+
+const BenchPlayers = () => {
+  return (
+    <Grid.Row>
+      <Grid>
+        <Grid.Column>
+        </Grid.Column>
+      </Grid>
+    </Grid.Row>
+  )
+}
+
+export default BenchPlayers
