2a7afe8ba68b26be986bff3ab6b3bc6bea85d81f
ellymaimon <eliranmaimon@gmail.com>
2018-11-30 17:53:55 -0800

    Add Away Team UI skeleton

--- src/AwayTeam.jsx
+++ src/AwayTeam.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Segment, Header } from 'semantic-ui-react';
 
 const AwayTeam = () => {
   return (
-    <div>
-      
-    </div>
+    <Segment>
+      <Header>Away Team</Header>
+    </Segment>
   )
 }
 
