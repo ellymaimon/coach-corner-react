1277985f71eb706fc27bf15e209d9f5c79d003be
ellymaimon <eliranmaimon@gmail.com>
2019-02-16 21:50:11 -0800

    Adjust CSS grid

--- src/game/GameScreen.jsx
+++ src/game/GameScreen.jsx
@@ -5,22 +5,38 @@ import { teams } from '../teamGenerator';
 
 const GameGrid = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: auto auto;
+  grid-template-rows: auto auto;
   grid-gap: 10px;
+`;
+
+const Scoreboard = styled.div`
+  grid-row: 1;
+`;
+
+const HomeTeam = styled.div`
+  grid-row: 2;
+  grid-column: 1;
+`;
+
+const AwayTeam = styled.div`
+  grid-row: 2;
+  grid-column: 2;
 `;
 
 const GameScreen = () => {
   return (
     <div style={{ maxWidth: 900, margin: '0 20px' }}>
       <GameGrid>
-        <div>
+        <Scoreboard>Scoreboard</Scoreboard>
+        <HomeTeam>
           <h2>Home Team</h2>
           <Team team={teams.home} />
-        </div>
-        <div>
+        </HomeTeam>
+        <AwayTeam>
           <h2>Away Team</h2>
           <Team team={teams.away} />
-        </div>
+        </AwayTeam>
       </GameGrid>
     </div>
   );
