1aaf979835dea772d6b196667dbca8cd3a0679d5
ellymaimon <eliranmaimon@gmail.com>
2019-02-18 15:47:47 -0800

    Add game logic function with game object

--- src/game-logic/gameLogic.js
+++ src/game-logic/gameLogic.js
@@ -5,3 +5,23 @@ console.log(teams);
 const getRandom = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
+const gameLogic = teams => {
+  let currentGame = {
+    homeTeam: {
+      active: teams.home.active,
+      bench: teams.home.bench,
+      timeouts: 2,
+    },
+    awayTeam: {
+      active: teams.away.active,
+      bench: teams.away.bench,
+      timeouts: 2,
+    },
+    currentQuarter: 1,
+    homeScore: 0,
+    awayScore: 0,
+    minutesRemaining: 12,
+    secondsRemaining: 0,
+  };
+};
