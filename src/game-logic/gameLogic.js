141ce1d219432e7d7585a2b7cc6329e6be6e411a
ellymaimon <eliranmaimon@gmail.com>
2019-02-18 15:48:53 -0800

    Add scoring function

--- src/game-logic/gameLogic.js
+++ src/game-logic/gameLogic.js
@@ -34,3 +34,18 @@ const gameOver = game => {
     ? true
     : false;
 };
+
+const attemptScore = (offensivePlayer, defensivePlayer) => {
+  let offenseRating = offensivePlayer.offense;
+  let missChance = getRandom(0, 100);
+
+  if (offenseRating > defensivePlayer.defense) {
+    if (Math.floor(offenseRating / 10) === Math.floor(missChance / 10)) {
+      return 3;
+    } else if (offenseRating > missChance) {
+      return 2;
+    } else {
+      return 0;
+    }
+  }
+};
