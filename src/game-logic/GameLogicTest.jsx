39e179ac40dd4982b954ba1283b4d99ee3fc2a9c
ellymaimon <eliranmaimon@gmail.com>
2019-02-20 20:22:53 -0800

    Add temporary initial game state

--- src/game-logic/GameLogicTest.jsx
+++ src/game-logic/GameLogicTest.jsx
@@ -1,6 +1,25 @@
 import React from 'react';
 
 const GameLogicTest = () => {
+  let initialGameState = {
+    homeTeam: {
+      active: teams.home.active,
+      bench: teams.home.bench,
+      timeouts: 2,
+      points: 0,
+    },
+    awayTeam: {
+      active: teams.away.active,
+      bench: teams.away.bench,
+      timeouts: 2,
+      points: 0,
+    },
+    currentQuarter: 1,
+    minutesRemaining: 12,
+    secondsRemaining: 0,
+    isOver: false,
+    isPaused: false,
+  };
   return (
     <div>
       game logic test
