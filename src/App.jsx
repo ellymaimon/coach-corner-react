61715a714216a96cf894f422131b02b6e72983b1
ellymaimon <eliranmaimon@gmail.com>
2019-03-03 12:04:26 -0800

    Apply app-wide styles reset and font family and size normalization

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React, { useState } from 'react';
 import SplashScreen from './splash/SplashScreen';
 // import GameScreen from './game/GameScreen';
 import Scoreboard from './game-logic/Scoreboard';
+import './App.css';
 
 const App = () => {
-  const [game, setGame] = useState(false);
+  const [game, setGame] = useState(true);
 
   return (
-    <div style={{ marginTop: '2em' }}>
+    <div className='app' style={{ marginTop: '2em' }}>
       {/* {game ? <GameScreen /> : <SplashScreen setGame={setGame} />} */}
       {game ? <Scoreboard /> : <SplashScreen setGame={setGame} />}
     </div>
