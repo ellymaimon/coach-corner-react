1fe193925532c2f92c482ab010a9ef196522a1f8
ellymaimon <eliranmaimon@gmail.com>
2019-03-03 12:04:53 -0800

    Restructure Active Player component to allow for CSS grid organization

--- src/game/ActivePlayers.jsx
+++ src/game/ActivePlayers.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import Player from './Player';
+import './ActivePlayers.css';
 
 const ActivePlayers = ({ activePlayers }) => {
   return (
-    <div style={{ marginBottom: '1em'}}>
+    <div style={{ marginBottom: '1em' }}>
       <h4>Active Players</h4>
-      {activePlayers.map(player => (
-        <Player key={player.id} player={player} />
-      ))}
+      <div className='category-row'>
+        <div>Name</div>
+        <div>Offense</div>
+        <div>Defense</div>
+        <div>Stamina</div>
+        <div>Points</div>
+      </div>
+        {activePlayers.map(player => (
+          <Player key={player.id} player={player} />
+        ))}
     </div>
   );
 };
