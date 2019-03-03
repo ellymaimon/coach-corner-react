f4f1e43c33f639be165406442c11782939cebbde
ellymaimon <eliranmaimon@gmail.com>
2019-03-03 15:13:36 -0800

    Add stops and FG% columns to player components

--- src/game/Player.jsx
+++ src/game/Player.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import './Player.css';
 
 const Player = ({ player }) => {
-  const { firstName, lastName, offense, defense, stamina, points } = player;
+  const { firstName, lastName, offense, defense, stamina, points, stops, fieldGoalPercentage } = player;
   return (
     <div className='player-container'>
       <div>
         {firstName} {lastName}
       </div>
-      <div>O: {offense}</div>
-      <div>D: {defense}</div>
-      <div>S: {stamina}</div>
-      <div>Pts: {points}</div>
+      <div>{offense}</div>
+      <div>{defense}</div>
+      <div>{stamina}</div>
+      <div>{points}</div>
+      <div>{stops}</div>
+      <div>{Math.round(fieldGoalPercentage)}%</div>
     </div>
   );
 };
