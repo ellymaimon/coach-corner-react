b03f95810dbc3957b60fe611b7111d27fea2be48
ellymaimon <eliranmaimon@gmail.com>
2019-03-03 12:03:12 -0800

    Restructure Player component to implement CSS grid

--- src/game/Player.jsx
+++ src/game/Player.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import './Player.css';
 
 const Player = ({ player }) => {
   const { firstName, lastName, offense, defense, stamina, points } = player;
   return (
-    <div>
-      {firstName} {lastName} Offense: {offense} Defense: {defense} Stamina:{' '}
-      {stamina} Points: {points}
+    <div className='player-container'>
+      <div>
+        {firstName} {lastName}
+      </div>
+      <div>O: {offense}</div>
+      <div>D: {defense}</div>
+      <div>S: {stamina}</div>
+      <div>Pts: {points}</div>
     </div>
   );
 };
