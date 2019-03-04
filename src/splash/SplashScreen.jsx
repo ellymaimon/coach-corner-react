b1388dee44aa60eff3bf186b88462eef11e14949
ellymaimon <eliranmaimon@gmail.com>
2019-03-04 14:47:15 -0800

    Remove styling done with  styled components

--- src/splash/SplashScreen.jsx
+++ src/splash/SplashScreen.jsx
@@ -1,24 +1,13 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const Container = styled.div`
-  max-width: 600px;
-  margin: 0 auto;
-`;
-
-const Button = styled.button`
-  background-color: #008cba;
-  color: white;
-  padding: 1em 2em;
-  font-size: 1em;
-`;
 
 const SplashScreen = ({ setGame }) => {
   return (
-    <Container>
+    <div className='container'>
       <h1>Coach's Corner!</h1>
-      <Button onClick={() => setGame(true)}>Play!</Button>
-    </Container>
+      <button className='btn' onClick={() => setGame(true)}>
+        Play!
+      </button>
+    </div>
   );
 };
 
