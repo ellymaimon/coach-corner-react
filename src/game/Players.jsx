03fde2329294f9051665f43a0ebca8ea914a4f63
ellymaimon <eliranmaimon@gmail.com>
2019-03-10 10:39:38 -0700

    Add status and endurance the players components

--- src/game/Players.jsx
+++ src/game/Players.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import Player from './Player';
 import './Players.css';
 
-const Players = ({ players }) => {
+const Players = ({ players, status }) => {
   return (
     <div>
+      <h2 className='players-header'>{status} Players</h2>
       <div className='category-row'>
         <div>Name</div>
         <div>Offense</div>
         <div>Defense</div>
+        <div>Endurance</div>
         <div>Stamina</div>
         <div>Points</div>
         <div>Stops</div>
