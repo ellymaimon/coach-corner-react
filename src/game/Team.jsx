042a657e778d5e19c24c825feec281ed9c16b19d
ellymaimon <eliranmaimon@gmail.com>
2019-03-11 20:08:33 -0700

    Implement logic for substituting players

--- src/game/Team.jsx
+++ src/game/Team.jsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import Game from '../game-logic/GameStore';
 import Players from './Players';
 
 const Team = ({ team }) => {
+  const game = useContext(Game);
+  const [subOut, setSubOut] = useState(null);
+  const [subIn, setSubIn] = useState(null);
+
+  const subPlayers = () => {
+    if (subOut !== null && subIn !== null) {
+      let subOutIndex = team.active.findIndex(
+        player => player.id === subOut.id
+      );
+      let subInIndex = team.bench.findIndex(player => player.id === subIn.id);
+
+      [team.active[subOutIndex], team.bench[subInIndex]] = [
+        team.bench[subInIndex],
+        team.active[subOutIndex],
+      ];
+    }
+    game.gameStart();
+  };
+
   return (
     <div>
-      <Players players={team.active} status={'Active'}/>
-      {/* <Players players={team.bench} /> */}
+      {subIn !== null && subOut !== null && game.state.allowSubs && (
+        <button onClick={() => subPlayers()}>Complete Substitution</button>
+      )}
+      {subOut && game.state.allowSubs && <p>Player selected to come out: {subOut.firstName} {subOut.lastName}</p>}
+      {subIn && game.state.allowSubs && <p>Player selected to come in: {subIn.firstName} {subIn.lastName}</p>}
+      <Players
+        players={team.active}
+        status={'Active'}
+        setSub={setSubOut}
+        subOut={subOut}
+      />
+      <Players
+        players={team.bench}
+        status={'Bench'}
+        setSub={setSubIn}
+        subIn={subIn}
+      />
     </div>
   );
 };
