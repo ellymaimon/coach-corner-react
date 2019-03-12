56a05315b07255ef30721ee10aca845d958a0d1c
ellymaimon <eliranmaimon@gmail.com>
2019-03-11 20:07:23 -0700

    Implement substitutions

--- src/game/Player.jsx
+++ src/game/Player.jsx
@@ -1,20 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import Game from '../game-logic/GameStore';
 import './Player.css';
 
-const Player = ({ player }) => {
-  const { firstName, lastName, offense, defense, stamina, endurance, points, stops, fieldGoalPercentage } = player;
+const Player = ({ player, setSub, subOut, subIn }) => {
+  const game = useContext(Game);
+  const [selected, setSelected] = useState(false);
+
+  useEffect(() => {
+    if (
+      (subOut && subOut.id === player.id && game.state.allowSubs) ||
+      (subIn && subIn.id === player.id && game.state.allowSubs)
+    ) {
+      setSelected(true);
+    } else {
+      setSelected(false);
+    }
+  }, [subOut, subIn, game.state.allowSubs]);
+
+  const {
+    firstName,
+    lastName,
+    offense,
+    defense,
+    stamina,
+    endurance,
+    points,
+    stops,
+    fieldGoalPercentage,
+  } = player;
+
   return (
-    <div className='player-container'>
-      <div>
-        {firstName} {lastName}
-      </div>
-      <div>{offense}</div>
-      <div>{defense}</div>
-      <div>{endurance}</div>
-      <div>{stamina}</div>
-      <div>{points}</div>
-      <div>{stops}</div>
-      <div>{Math.round(fieldGoalPercentage)}%</div>
+    <div>
+      {selected ? (
+        <div className='player-container selected'>
+          <div>
+            {firstName} {lastName}
+          </div>
+          <div>{offense}</div>
+          <div>{defense}</div>
+          <div>{endurance}</div>
+          <div>{stamina}</div>
+          <div>{points}</div>
+          <div>{stops}</div>
+          <div>{Math.round(fieldGoalPercentage)}%</div>
+        </div>
+      ) : (
+        <div
+          className='player-container'
+          onClick={() => {
+            game.state.allowSubs && setSub(player);
+          }}
+        >
+          <div>
+            {firstName} {lastName}
+          </div>
+          <div>{offense}</div>
+          <div>{defense}</div>
+          <div>{endurance}</div>
+          <div>{stamina}</div>
+          <div>{points}</div>
+          <div>{stops}</div>
+          <div>{Math.round(fieldGoalPercentage)}%</div>
+        </div>
+      )}
     </div>
   );
 };
